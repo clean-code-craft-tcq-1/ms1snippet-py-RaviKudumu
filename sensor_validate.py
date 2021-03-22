4137f203397a38304a7aa30d47bd58211bad7b9a
Ravi Kudumu <ravikudumu@gmail.com>
2021-03-22 20:22:31 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -6,9 +6,12 @@ def sensor_working_properly(value, nextValue, maxDelta):
     return True
 
 def is_sensor_reading_proper(values, sensor_name):
-    values = [i for i in values if i]
+    values = remove_none_values(values)
     last_but_one_reading = len(values) - 1
     for i in range(last_but_one_reading):
         if(not sensor_working_properly(values[i], values[i + 1], sensor_max_difference[sensor_name])):
             return False
     return True
+def remove_none_values(values):
+    values = [i for i in values if i]
+    return values
