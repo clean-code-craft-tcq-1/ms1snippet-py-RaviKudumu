f9d881c9235e735c9476169f7652208ea9588e23
Ravi Kudumu <ravikudumu@gmail.com>
2021-03-22 20:13:22 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,19 +1,14 @@
+sensor_max_difference = { 'soc': 0.05, 'current' : 0.1 }
 
-def  _give_me_a_good_name(value, nextValue, maxDelta):
-  if nextValue - value > maxDelta:
-    return False
-  return True
+def sensor_working_properly(value, nextValue, maxDelta):
+    if nextValue - value > maxDelta:
+        return False
+    return True
 
-def validate_soc_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
-      return False
-  return True
-
-def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
-      return False
-  return True
+def is_sensor_reading_proper(values, sensor_name):
+    values = [i for i in values if i]
+    last_but_one_reading = len(values) - 1
+    for i in range(last_but_one_reading):
+        if(not sensor_working_properly(values[i], values[i + 1], sensor_max_difference[sensor_name])):
+            return False
+    return True
